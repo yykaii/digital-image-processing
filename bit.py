c0f5dbaa14aee3eaed5a578aa810eaab9397ae7d
zhangwei <zzzwei@cn.ibm.com>
2019-06-26 14:35:43 +0800

    bit layer split

--- bit.py
+++ bit.py
@@ -2,6 +2,8 @@ import cv2
 import os
 from matplotlib import pyplot as plt
 from skimage import measure, color, morphology
+import numpy as np
+import math
 #Done
 
 isShowImage = True
@@ -12,7 +14,7 @@ def showCV2Image(title, img):
     cv2.waitKey(0)
 
 if __name__ == '__main__':
-        image = cv2.imread('2.jpg')
+        image = cv2.imread('Johns_DL.jpg')
         if isShowImage:
             showCV2Image('image', image)
 
@@ -20,7 +22,25 @@ if __name__ == '__main__':
         if isShowImage:
             showCV2Image('gray', gray)
 
+        rows, cols = gray.shape
+
+        #比特分层
+        bits = []
         for i in range(8):
-            ret, binary = cv2.threshold(gray, pow(2, i)/2-1, 255, cv2.THRESH_BINARY)
+            ret, binary = cv2.threshold(gray, pow(2, i)-1, 255, cv2.THRESH_BINARY)
+            bits.append(binary)
             if isShowImage:
-                showCV2Image('binary', binary)
+                showCV2Image('binary'+str(i), binary)
+
+        #重建，4个高层的比特面即可很好重建
+        bits.reverse()
+
+        re = bits[0]
+        for s in range(4):
+            re = cv2.bitwise_and(bits[s], re)
+
+        if isShowImage:
+            showCV2Image('re', re)
+
+        cv2.imwrite('Johns_DL_bit.jpg', re)
+
